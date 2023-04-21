78121e62af44ef406d1813443c110a6103750a29
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-04-21 10:23:32 +0200

    revent

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 // -- slider  -- //
 
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    loop: true,
-    keyboard: {
-    enabled: true,
-  },
-    navigation: {
-      nextEl: ".button-next",
-      prevEl: ".button-prev",
-  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  keyboard: {
+  enabled: true,
+},
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+},
 
-    pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
-        renderFraction: function (currentClass, totalClass) {
-            return '<span class="' + currentClass + '"></span>' +
-            ' of ' +
-            '<span class="' + totalClass + '"></span>'; 
-        },
-    },
-  });
+  pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+      renderFraction: function (currentClass, totalClass) {
+          return '<span class="' + currentClass + '"></span>' +
+          ' of ' +
+          '<span class="' + totalClass + '"></span>'; 
+      },
+  },
+});
