32c6a0a98a7ec3d3e8289eb7dc613f56fcc62ca5
Alla-don <116377525+Alla-don@users.noreply.github.com>
2023-04-24 00:57:39 +0300

    fixed styles

--- src/main.js
+++ src/main.js
@@ -1,24 +1,45 @@
 // -- slider  -- //
 
-const swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper('.mySwiper', {
   slidesPerView: 1,
   spaceBetween: 10,
   loop: true,
   keyboard: {
-  enabled: true,
-},
+    enabled: true,
+  },
   navigation: {
-    nextEl: ".button-next",
-    prevEl: ".button-prev",
-},
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
 
-    pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
-        renderFraction: function (currentClass, totalClass) {
-            return '<span class="' + currentClass + '"></span>' +
-            ' of ' +
-            '<span class="' + totalClass + '"></span>'
-        },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+    renderFraction: function (currentClass, totalClass) {
+      return (
+        '<span class="' +
+        currentClass +
+        '"></span>' +
+        ' of ' +
+        '<span class="' +
+        totalClass +
+        '"></span>'
+      );
     },
-  });
+  },
+});
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.menu-open'),
+    closeMenuBtn: document.querySelector('.menu-close'),
+    menu: document.querySelector('.mobile-menu'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+})();
